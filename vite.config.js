79e07b47594101538513bdbe1853a53173d6bcc7
Yang <D1349375@o365.fcu.edu.tw>
2026-06-08 13:08:22 +0800

    設定 vite config 的 base 路徑解決白畫面

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 🌟 這是解決白畫面的關鍵！必須跟你的 GitHub Repository 名稱完全一模一樣
+  base: '/vue-sfc/', 
+  plugins: [vue()],
+})
