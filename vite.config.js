bb1a4589ade04eab386c4d6b9f1cac80c59f403b
Yang <D1349375@o365.fcu.edu.tw>
2026-06-09 18:36:10 +0800

    修正 index.html 路徑讓 Vite 正常執行打包

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 🌟 這是解決白畫面的關鍵！必須跟你的 GitHub Repository 名稱完全一模一樣
-  base: './', 
+  base: '/vue-sfc/', 
   plugins: [vue()],
 })
